e00cc9f03ae2fba88af83f09ca86fd4eb151da32
cttynul <cttynul@outlook.com>
2025-08-20 18:47:26 +0200

    updated netlify function

--- functions/m3u-generator.js
+++ functions/m3u-generator.js
@@ -40,7 +40,7 @@ const groupsMap = {
     'ie': ['Ireland'],
     'il': ['Israel'],
     'it': ['Italy', 'VOD Italy'],
-    'jp': ['æ—¥æœ¬/Japan'],
+    'jp': ['Japan'],
     'kr': ['Korea'],
     'xk': ['Kosovo'],
     'lv': ['Latvia'],
@@ -88,7 +88,9 @@ const groupsMap = {
     'all': []
 };
 
+// Funzione principale della Netlify Function
 exports.handler = async (event) => {
+    // Estrae il codice del paese dai parametri del percorso
     const pathSegments = event.path.split('/').filter(Boolean);
     const lastSegment = pathSegments[pathSegments.length - 1];
 
@@ -97,16 +99,101 @@ exports.handler = async (event) => {
         countryCode = lastSegment.toLowerCase();
     }
 
-    // Se non viene specificato un codice paese, restituisci un errore
+    // Se non viene specificato un codice paese, reindirizza alla pagina di aiuto
     if (!countryCode) {
         return {
-            statusCode: 404,
+            statusCode: 302,
             headers: {
-                'Content-Type': 'text/plain; charset=utf-8',
-                'Access-Control-Allow-Origin': '*'
+                'Location': `${event.path}/help`,
             },
-            body: 'Error: Country code not provided. Please use the format /api/[country_code].',
+            body: ''
         };
+    }
+    
+    // Gestione della richiesta 'help'
+    if (countryCode === 'help') {
+        const helpText = `
+opentivu m3u8 generator API
+--------------------------
+
+This API generates a filtered M3U8 playlist based on a country code or provides EPG data.
+
+Usage:
+/api/[country_code]
+
+Parameters:
+[country_code] - A two-letter country code (e.g., 'it' for Italy) or 'epg' for EPG data.
+
+Available country codes:
+${Object.keys(groupsMap).map(code => ` - ${code.toUpperCase()}`).join('\n')}
+ - EPG
+
+Example:
+To get all Italian channels, use: /api/it
+To get all available channels, use: /api/all
+To get the combined EPG data, use: /api/epg
+
+Note: This API is for personal use and provides a list of publicly and free-to-air available TV streams.
+        `;
+
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'text/plain' },
+            body: helpText,
+        };
+    }
+
+    // Gestione della richiesta 'epg'
+    if (countryCode === 'epg') {
+        const italianEpgUrl = 'https://tvit.leicaflorianrobert.dev/epg/list.xml';
+        const plutoEpgUrl = 'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/it.xml';
+
+        try {
+            const [italianEpgResponse, plutoEpgResponse] = await Promise.all([
+                fetch(italianEpgUrl),
+                fetch(plutoEpgUrl)
+            ]);
+
+            if (!italianEpgResponse.ok || !plutoEpgResponse.ok) {
+                throw new Error('Errore durante il recupero di uno o entrambi i file EPG.');
+            }
+
+            const italianEpgXml = await italianEpgResponse.text();
+            const plutoEpgXml = await plutoEpgResponse.text();
+
+            const extractContent = (xmlString) => {
+                return xmlString.replace(/<\?xml[^>]*\?>/g, '')
+                                .replace(/<!DOCTYPE[^>]*>/g, '')
+                                .replace(/<tv[^>]*>|<\/tv>/g, '')
+                                .trim();
+            };
+
+            const italianContent = extractContent(italianEpgXml);
+            const plutoContent = extractContent(plutoEpgXml);
+            
+            const combinedEpg = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tv SYSTEM "xmltv.dtd">
+<tv>
+${italianContent}
+${plutoContent}
+</tv>`;
+
+            return {
+                statusCode: 200,
+                headers: {
+                    'Content-Type': 'application/xml',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: combinedEpg,
+            };
+
+        } catch (error) {
+            console.error('Errore durante l\'unione dei file EPG:', error);
+            return {
+                statusCode: 500,
+                body: 'Errore interno del server durante l\'unione dei file EPG.'
+            };
+        }
     }
     
     // Controlla se il codice paese è valido
@@ -120,6 +207,8 @@ exports.handler = async (event) => {
 
     const m3u8Url = 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8';
     const italianEpgUrl = 'https://tvit.leicaflorianrobert.dev/epg/list.xml';
+    const plutoEpgUrl = 'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/it.xml';
+
 
     try {
         const response = await fetch(m3u8Url);
@@ -138,11 +227,15 @@ exports.handler = async (event) => {
                 const groupTitleMatch = line.match(/group-title="([^"]*)"/);
                 const groupTitle = groupTitleMatch ? groupTitleMatch[1] : '';
 
+                const channelNameMatch = line.match(/,(.*)$/);
+                const originalChannelName = channelNameMatch ? channelNameMatch[1].trim() : '';
+
                 if (lines[i + 1] && lines[i + 1].startsWith('http')) {
                     channels.push({
                         meta: line,
                         url: lines[i + 1],
-                        group: groupTitle
+                        group: groupTitle,
+                        name: originalChannelName
                     });
                     i++;
                 }
@@ -171,12 +264,23 @@ exports.handler = async (event) => {
             filteredChannels = channels.filter(channel => allowedGroups.includes(channel.group));
         }
 
-        // Aggiungi il tag EPG solo se il codice del paese è 'it'
-        const header = (countryCode === 'it') ? `#EXTM3U url-tvg="${italianEpgUrl}"\n` : '#EXTM3U\n';
+        const header = (countryCode === 'it') 
+            ? `#EXTM3U url-tvg="${italianEpgUrl},${plutoEpgUrl}"\n` 
+            : '#EXTM3U\n';
         
         let filteredM3u = header;
         filteredChannels.forEach(channel => {
-            filteredM3u += channel.meta + '\n' + channel.url + '\n';
+            // Funzione per rimuovere i caratteri speciali, gli indicatori e la stringa " - Pluto TV"
+            const cleanName = channel.name
+                .replace(/ \u24d8|\u24bc|\u24c8|\u24df|\u24e2|\u24d5|\u24e6|\u24d1|\u24e5|\u24dc|\u24d0|\u24e7|\u24d9|\u24d7|\u24e8|\u24d4|\u24e4|\u24dd|\u24d6|\u24e1|\u24e3|\u24db|\u24da|\u24e0|\u24de|\u24e9/g, '')
+                .replace(/ – Pluto TV/g, '') // Rimuove " - Pluto TV"
+                .trim();
+
+            let newMeta = channel.meta.replace(/tvg-id="[^"]*"/, `tvg-id="${cleanName}"`);
+            newMeta = newMeta.replace(/tvg-name="[^"]*"/, `tvg-name="${cleanName}"`);
+            newMeta = newMeta.replace(/,.*$/, `,${cleanName}`);
+
+            filteredM3u += newMeta + '\n' + channel.url + '\n';
         });
 
         return {
